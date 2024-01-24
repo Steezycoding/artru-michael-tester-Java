845f1ea801b3cd0e100d958b79092b3b833eea42
Michael "Steezy" A <38781991+steezycoding@users.noreply.github.com>
2024-01-24 14:27:07 +0100

    :white_check_mark: Ticket saving test (recurrent user)

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -9,6 +9,8 @@ import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.testutil.DateUtility;
+import com.parkit.parkingsystem.testutil.TimeSlot;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,8 +30,8 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
-    private static Date actualDateMinusOneHour;
 
+    private static Date actualDate;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -45,12 +47,9 @@ public class ParkingDataBaseIT {
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
+        // Date : 2024-01-10 13:00:00.000
+        actualDate = new Date(1704888000000L);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.HOUR_OF_DAY, -1);
-        actualDateMinusOneHour = calendar.getTime();
     }
 
     @AfterEach
@@ -63,7 +62,7 @@ public class ParkingDataBaseIT {
         when(inputReaderUtil.readSelection()).thenReturn(1);
 
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
+        parkingService.processIncomingVehicle(actualDate);
 
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
 
@@ -71,49 +70,69 @@ public class ParkingDataBaseIT {
         assertThat(ticket.getVehicleRegNumber()).isEqualTo("ABCDEF");
         assertThat(ticket.getPrice()).isEqualTo(0);
         assertThat(ticket.getParkingSpot().getParkingType()).isEqualTo(ParkingType.CAR);
-        assertThat(ticket.getInTime()).isCloseTo(new Date(), 1000);
+        assertThat(ticket.getInTime().toInstant()).isEqualTo(actualDate.toInstant());
         assertThat(ticket.getOutTime()).isNull();
         assertThat(ticket.getParkingSpot().isAvailable()).isFalse();
     }
 
     @Test
     public void testParkingLotExit() {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
         Ticket ticket = new Ticket();
         ticket.setVehicleRegNumber("ABCDEF");
-        ticket.setInTime(actualDateMinusOneHour);
-        ticket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
-
+        ticket.setInTime(DateUtility.dateModifier(actualDate, TimeSlot.HOUR, -1));
+        ticket.setParkingSpot(parkingSpot);
+        parkingSpotDAO.updateParking(parkingSpot);
         ticketDAO.saveTicket(ticket);
 
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
+        parkingService.processExitingVehicle(actualDate);
 
         Ticket updatedTicket = ticketDAO.getTicket("ABCDEF");
 
         assertThat(updatedTicket.getPrice()).isEqualTo(Fare.CAR_RATE_PER_HOUR);
-        assertThat(updatedTicket.getOutTime()).isCloseTo(new Date(), 1000);
-        assertThat(ticket.getParkingSpot().isAvailable()).isTrue();
+        assertThat(updatedTicket.getOutTime().toInstant()).isEqualTo(actualDate.toInstant());
+        assertThat(updatedTicket.getParkingSpot().isAvailable()).isTrue();
     }
 
+    /*
+        WARNING
+        Creating only 2 tickets requires the UserRecurrence.MIN_TICKET_COUNT
+        constant to be set to 2!
+        We need to :
+            (either) Create as many tickets as necessary to match the MIN_TICKET_COUNT (loop)
+            (or) Pass the MIN_TICKET_COUNT constant to ParkingService()
+                 so that it can be set to the desired number for testing purposes
+     */
+    // TODO: fix the problem mentioned in the comment above
     @Test
     public void testParkingLotExitRecurringUser() {
         Ticket ticketOne = new Ticket();
         ticketOne.setVehicleRegNumber("ABCDEF");
         ticketOne.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, true));
-        ticketOne.setInTime(actualDateMinusOneHour);
-        ticketOne.setOutTime(new Date());
+        ticketOne.setInTime(DateUtility.dateModifier(actualDate, TimeSlot.HOUR, -3));
+        ticketOne.setOutTime(DateUtility.dateModifier(actualDate, TimeSlot.HOUR, -2));
         ticketOne.setPrice(Fare.CAR_RATE_PER_HOUR);
         ticketDAO.saveTicket(ticketOne);
 
         Ticket ticketTwo = new Ticket();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
         ticketTwo.setVehicleRegNumber("ABCDEF");
-        ticketTwo.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
-        ticketTwo.setInTime(actualDateMinusOneHour);
+        ticketTwo.setParkingSpot(parkingSpot);
+        ticketTwo.setInTime(DateUtility.dateModifier(actualDate, TimeSlot.HOUR, -1));
+        parkingSpotDAO.updateParking(parkingSpot);
         ticketDAO.saveTicket(ticketTwo);
+
+
 
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
-        parkingService.processExitingVehicle();
+        parkingService.processExitingVehicle(actualDate);
+
         Ticket lastTicket = ticketDAO.getTicket("ABCDEF");
+
+        assertThat(lastTicket.getPrice()).isEqualTo(Fare.CAR_RATE_PER_HOUR * 0.95);
+        assertThat(lastTicket.getOutTime().toInstant()).isEqualTo(actualDate.toInstant());
+        assertThat(lastTicket.getParkingSpot().isAvailable()).isTrue();
     }
 }
---
+++ src/test/java/com/parkit/parkingsystem/testutil/TimeSlot.java
@@ -0,0 +1,5 @@
+package com.parkit.parkingsystem.testutil;
+
+public enum TimeSlot {
+	HOUR
+}
